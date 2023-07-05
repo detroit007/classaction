ecf8c2a4b56cab08844d2ed28457a63820280c45
Hamza Yousaf <hamzayousaf056@gmail.com>
2023-07-05 13:40:23 +0500

    home page design

--- components/pages/QuotesSection/page.jsx
+++ components/pages/QuotesSection/page.jsx
@@ -10,6 +10,7 @@ export function QuotesSection(){
           <div className="slide-wrapper flex items-center cursor-pointer w-2/3">
             { [...quotes, ...quotes, ...quotes]?.map((item, i) => (
                   <img
+                    key={i}
                     // className={` max-w-70 md:max-w-80 mix-blend-multiply mx-auto`}
                     className={`slide`}
                     src={item}
